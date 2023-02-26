dd555d553b4c891c0e702980c9522d1be5e19a0d
semen <kalyakulin.semen@gmail.com>
2023-02-26 22:30:49 +0700

    update

--- js/contacts.js
+++ js/contacts.js
@@ -8,6 +8,7 @@ fetch('http://127.0.0.1:8000/personal_info/contact_details/1/')
         document.getElementById("street").innerHTML = "улица " + data.street_name + ", " + data.street_number;
         document.getElementById("postcode").innerHTML = data.postcode;
         document.getElementById("office_number").innerHTML = "Офис " + data.office_number;
+        document.getElementById("link_to_office_photo").setAttribute('src', data.link_to_office_picture);
         document.getElementById("telephone_extension").innerHTML = "Внутренний телефон " + data.telephone_extension;
         document.getElementById("work_phone_number").innerHTML = "Рабочий: " + data.work_phone_number;
         document.getElementById("additional_phone_number").innerHTML = "Дополнительный : " + data.additional_phone_number;
@@ -24,7 +25,7 @@ fetch('http://127.0.0.1:8000/personal_info/contact_details/1/')
 fetch('http://127.0.0.1:8000/personal_info/main_info/1/')
     .then(response => response.json())
     .then(data => {
-            console.log(data) // check data in console
+        console.log(data) // check data in console
         document.getElementById("FIO").innerHTML = data.last_name + ' ' + data.first_name;
         document.getElementById("FIO_footer").innerHTML = data.last_name + ' ' + data.first_name;
         document.getElementById("site_description").innerHTML = data.department_name;
